0d338c5f21d1700be6ba5ad736c6f2b7bbb1d522
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2024-03-20 13:39:36 +0100

    :wrench: test defines and options.

--- MathFunctions.cxx
+++ MathFunctions.cxx
@@ -1,3 +1,14 @@
+#if !defined(TIPI_TEAM)
+  #error TIPI_TEAM was not forwarded as define
+#endif
+
+#if !defined(TIPI_TEAM_ZURICH)
+  #error TIPI_TEAM was not forwarded as define
+#endif
+
+
+
+
 #include "MathFunctions.h"
 
 #include <cmath>
