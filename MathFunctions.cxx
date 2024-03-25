9652ce2756ae45f7124ba250ce9d98b7192b6102
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2024-03-25 21:37:19 +0100

    :gear: test case with SOURCE_SUBDIR.

--- MathFunctions.cxx
+++ MathFunctions.cxx
@@ -1,14 +1,3 @@
-#if !defined(TIPI_TEAM)
-  #error TIPI_TEAM was not forwarded as define
-#endif
-
-#if !defined(TIPI_TEAM_ZURICH)
-  #error TIPI_TEAM was not forwarded as define
-#endif
-
-
-
-
 #include "MathFunctions.h"
 
 #include <cmath>
