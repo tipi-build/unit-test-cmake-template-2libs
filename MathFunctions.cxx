0ad97390f914cbb2a7bbdd035e2bcbd3ec1ac213
Yannic Staudt <yannic@tipi.build>
2024-11-27 20:50:44 +0100

    :wrench: making sure that the HfcDependencyProvidedLib is used

--- MathFunctions.cxx
+++ MathFunctions.cxx
@@ -1,10 +1,12 @@
 #include "MathFunctions.h"
 
 #include <cmath>
+#include <lib.h> // straight out of HfcDependencyProvidedLib
 
 namespace MathFunctions {
 double sqrt(double x)
 {
+  print_hello();
   return std::sqrt(x);
 }
 }
